04ef26e695433021c4aa11c2c6fe71596def9239
stonegr <74632994+stonegr@users.noreply.github.com>
2022-12-22 11:58:05 +0800

    Update config.py

--- tgbot/config.py
+++ tgbot/config.py
@@ -1,8 +1,11 @@
 # any configuration should be stored here
 
 TOKEN = ""  # configure env if you need;
+# 允许使用的用户id
 USER_ID = ""
+# aria2c 下载开始及完成的通知id
 NOTIFY_USER_ID = ""
+# tgbot的代理
 HTTP_PROXY = ""
 
 # aria2c
